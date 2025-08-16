1377081ec8c2f62179f0dd44662112fedaf20e84
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-08-15 23:27:18 -0300

    cria os atributos e construtor para armazenar os dados dos produtos da loja

--- src/entities/Loja.java
+++ src/entities/Loja.java
@@ -2,4 +2,15 @@ package entities;
 
 public class Loja {
 
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+	public Loja(String nome, double preco, int quantidade) {
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+	
+	
 }
